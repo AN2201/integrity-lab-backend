f56ce8a54e14dc243e58d21d856eaa042d87bad4
AN2201 <a.nekrutov@beter.co>
2025-04-01 18:13:16 +0300

    Update sessions.js
    Delete duplicate Session.js from root

--- sessions.js
+++ sessions.js
@@ -1,17 +1,1 @@
-const express = require('express');
-const router = express.Router();
-const Session = require('../models/Session');
 
-// Отримати всі сесії
-router.get('/', async (req, res) => {
-  const sessions = await Session.find().sort({ createdAt: -1 });
-  res.json(sessions);
-});
-
-// Отримати одну сесію по ID
-router.get('/:id', async (req, res) => {
-  const session = await Session.findById(req.params.id);
-  res.json(session);
-});
-
-module.exports = router;
